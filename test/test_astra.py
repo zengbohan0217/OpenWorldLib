79ef565682af59a9eee47b3ce77d199d38fbd23f
xh31563 <xh31563@gmail.com>
2026-01-21 02:01:52 +0800

    这里把上次说的问题都改掉了。

--- test/test_astra.py
+++ test/test_astra.py
@@ -1,19 +1,23 @@
 import os
+import sys
+current_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_path)
+project_root = os.path.dirname(current_dir)
+sys.path.append(project_root)
 import numpy as np
 from PIL import Image
-
 from src.sceneflow.pipelines.kling.pipeline_astra import AstraPipeline
 
 def test():
-    # 模型路径
-    DIT_PATH = "../models/Astra/checkpoints/diffusion_pytorch_model.ckpt"
-    WAN_MODEL_PATH = "../models/Wan-AI/Wan2.1-T2V-1.3B"
+    DIT_PATH = "EvanEternal/Astra" 
     
-    # 输入素材
-    CONDITION_IMAGE = "data.test_case1.ref_image.png"
+    # Wan2.1 的官方 ID 
+    WAN_MODEL_PATH = "Wan-AI/Wan2.1-T2V-1.3B" 
+    
+    CONDITION_IMAGE = os.path.join(project_root, "data/test_case1/ref_image.png")
     
     # 交互控制
-    PROMPT = "A first-person view walking through a beautiful garden."
+    PROMPT = "A sunlit European street lined with historic buildings and vibrant greenery creates a warm, charming, and inviting atmosphere. The scene shows a picturesque open square paved with red bricks, surrounded by classic narrow townhouses featuring tall windows, gabled roofs, and dark-painted facades. On the right side, a lush arrangement of potted plants and blooming flowers adds rich color and texture to the foreground. A vintage-style streetlamp stands prominently near the center-right, contributing to the timeless character of the street. Mature trees frame the background, their leaves glowing in the warm afternoon sunlight. Bicycles are visible along the edges of the buildings, reinforcing the urban yet leisurely feel. The sky is bright blue with scattered clouds, and soft sun flares enter the frame from the left, enhancing the scene’s inviting, peaceful mood."
     DIRECTION = "left"  # 可选: forward, backward, left, right, forward_left, s_curve
     
     # 输出路径
@@ -31,7 +35,7 @@ def test():
     
     # 执行生成
     # 所有的复杂参数（步数、CFG、滑动窗口）都在 Pipeline 的 DefaultConfig 里设置
-    pipeline.process(
+    pipeline(
         condition_image=CONDITION_IMAGE,
         prompt=PROMPT,
         direction=DIRECTION,
