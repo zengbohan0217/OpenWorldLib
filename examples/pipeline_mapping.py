79675a690bbbfa429394ba2e45356174bf97fb11
Ryann-Ran <yuranw829@gmail.com>
2026-02-23 16:39:46 +0800

    add hunyuan_game_craft evaluation

--- examples/pipeline_mapping.py
+++ examples/pipeline_mapping.py
@@ -6,6 +6,13 @@ def load_matrix_game2_pipeline(model_path, device):
             device=device,
             )
 
+def load_hunyuan_game_craft_pipeline(model_path, device):
+    from sceneflow.pipelines.hunyuan_world.pipeline_hunyuan_game_craft import HunyuanGameCraftPipeline
+    return HunyuanGameCraftPipeline.from_pretrained(
+            synthesis_model_path=model_path,
+            device=device,
+            )
+
 def load_qwen2p5_omni_pipeline(model_path, device):
     from sceneflow.pipelines.qwen.pipeline_qwen2p5_omni import Qwen2p5OmniPipeline
     return Qwen2p5OmniPipeline.from_pretrained(
@@ -18,6 +25,7 @@ def load_qwen2p5_omni_pipeline(model_path, device):
 ## utilize lazy loader to load different tasks pipeline
 video_gen_pipe = {
     "matrix-game2": load_matrix_game2_pipeline,
+    "hunyuan-game-craft": load_hunyuan_game_craft_pipeline,
 }
 
 reasoning_pipe = {
