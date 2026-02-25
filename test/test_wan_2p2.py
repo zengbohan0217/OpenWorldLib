774637f80eeaf321ef68fdbe9a68654efef74a39
dailihua <dl_h_1@126.com>
2026-02-25 15:24:35 +0800

    fix

--- test/test_wan_2p2.py
+++ test/test_wan_2p2.py
@@ -11,24 +11,28 @@ pretrained_model_path: str = "Wan-AI/Wan2.2-TI2V-5B"
 pipeline = Wan2p2Pipeline.from_pretrained(
     synthesis_model_path=pretrained_model_path,
     task="ti2v-5B",
+    size="1280*704",
+    prompt=(
+        "Summer beach vacation style, a white cat wearing sunglasses "
+        "sits on a surfboard..."
+    ),
+    image="",
+    save_file="./wan_app_demo_output.mp4",
+    base_seed=42,
     device_id=0,
     rank=0,
 )
 
 
 output_video = pipeline(
-    prompt=(
-        "Summer beach vacation style, a white cat wearing sunglasses "
-        "sits on a surfboard..."
-    ),
-    image_path="your path to image",
-    size="1280*704",
+    prompt=pipeline.prompt,
+    image_path=pipeline.image,
+    save=True,
 )
 
-save_file_path = "./wan_app_demo_output.mp4"
 save_video(
     tensor=output_video[None],
-    save_file=save_file_path,
+    save_file=pipeline.save_file,
     fps=WAN_CONFIGS[pipeline.task].sample_fps,
     nrow=1,
     normalize=True,
