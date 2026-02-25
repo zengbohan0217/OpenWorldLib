7436db853492d640e322f8339c1b6b833b0af671
aiihn <101506918+aiihn@users.noreply.github.com>
2026-02-25 14:31:33 +0800

    1. 首先test文件中，我们尽量不要让用户去找pipeline这个类之外的内容，from sceneflow.synthesis.visual_generation.hunyuan_world.hunyuan_worldplay.generate import save_video     from sceneflow.synthesis.visual_generation.hunyuan_world.hunyuan_worldplay.commons.infer_state import initialize_infer_state 这两部分尽量作为pipeline的子函数存在     2. 可以的话我想着注释要不别写了，比如src/sceneflow/synthesis/visual_generation/hunyuan_world/hunyuan_worldplay/models/autoencoders/__init__.py 这里这种，后续我们统一加License以及致谢吧     3. src/sceneflow/pipelines/hunyuan_world/__init__.py 这里的写法非常好，不过由于不同pipeline之前可能环境存在差异，我担心这种非lazyloader的形式会干扰相互的环境，这个文件还是去掉吧     4. operator还需要改一下，目前的 operator貌似都是在处理frame，准备perception的内容，这部分在模版中应该是 process_perception 下面的内容，而 process_interaction get_interaction check_interaction三个函数，是用来处理 test文件中interaction_signal = "w-10, right-10, d-11" 这部分的，确认interaction是否合法，可以参考一下hunyuan-gamecraft     5. 另外hunyuan_worldplay是基于hunyuan-video1.5，我在考虑这部分后续要不要加到base_model下面

--- test/test_hunyuan_worldplay.py
+++ test/test_hunyuan_worldplay.py
@@ -1,18 +1,5 @@
 import os
 from sceneflow.pipelines.hunyuan_world.pipeline_hunyuan_worldplay import HunyuanWorldPlayPipeline
-from sceneflow.synthesis.visual_generation.hunyuan_world.hunyuan_worldplay.generate import save_video
-from sceneflow.synthesis.visual_generation.hunyuan_world.hunyuan_worldplay.commons.infer_state import initialize_infer_state
-
-# Initialize infer state with default parameters
-initialize_infer_state(
-    sage_blocks_range="0-53",
-    use_sageattn=False,
-    enable_torch_compile=False,
-    use_fp8_gemm=False,
-    quant_type="fp8-per-block",
-    include_patterns="double_blocks",
-    use_vae_parallel=False
-)
 
 image_path = "./data/test_case/test_image_seq_case1/image_0001.jpg"
 prompt = "A paved pathway leads towards a stone arch bridge spanning a calm body of water."
@@ -30,12 +17,11 @@ pipeline = HunyuanWorldPlayPipeline.from_pretrained(
     enable_offloading=True,
     device="cuda"
 )
-
 output = pipeline(
     prompt=prompt,
     reference_image=image_path,
-    interaction_signal=interaction_signal
+    interaction_signal=interaction_signal,
 )
 
 save_video_path = os.path.join(output_path, "hunyuan_worldplay_demo.mp4")
-save_video(output.videos, save_video_path)
+HunyuanWorldPlayPipeline.save_video(output.videos, save_video_path)
